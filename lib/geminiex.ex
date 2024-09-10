a449de3376f08c4dcf37afb076e353faf11b332d
ontofractal <v@ontofractal.com>
2024-09-10 20:02:25 +0300

    init geminiex (∩｀-´)⊃━✿✿✿✿✿✿

---
+++ lib/geminiex.ex
@@ -0,0 +1,18 @@
+defmodule Geminiex do
+  @moduledoc """
+  Documentation for `Geminiex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Geminiex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geminiex_test.exs
@@ -0,0 +1,8 @@
+defmodule GeminiexTest do
+  use ExUnit.Case
+  doctest Geminiex
+
+  test "greets the world" do
+    assert Geminiex.hello() == :world
+  end
+end
