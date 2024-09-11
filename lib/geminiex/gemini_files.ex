fb25c0bca13c7d2294d93c8b60c116cc79adfc2b
ontofractal <v@ontofractal.com>
2024-09-11 15:52:16 +0300

    refactor gemini files

--- lib/geminiex/gemini_files.ex
+++ lib/geminiex/gemini_files.ex
@@ -3,31 +3,23 @@ defmodule GeminiAI.Files do
   A module for handling file operations with the Google AI file service.
   """
 
-  @base_url "https://generativelanguage.googleapis.com/v1beta"
+  alias GeminiAI.Files.Response
 
-  @type t :: %__MODULE__{
-          name: String.t(),
-          mimeType: String.t(),
-          size: non_neg_integer(),
-          displayName: String.t(),
-          createTime: String.t(),
-          updateTime: String.t()
-        }
+  @base_url "https://generativelanguage.googleapis.com/v1beta"
 
   @doc """
   Uploads a file to the Google AI file service.
 
   ## Examples
 
-      iex> GeminiAI.File.upload("path/to/image.jpg")
-      {:ok, %GeminiAI.File{...}}
-
+      iex> {:ok, file} = GeminiAI.Files.upload("path/to/image.jpg")
+      iex> %GeminiAI.Files.Response{name: name, mimeType: mime_type} = file
+      iex> String.starts_with?(name, "files/")
+      true
   """
-  @spec upload(String.t(), keyword()) :: {:ok, t()} | {:error, any()}
-  def upload(path, opts \\ []) do
-    api_key = Application.fetch_env!(:gemini_ai, :api_key)
-    url = "#{@base_url}/files"
-
+  @spec upload(Req.Request.t() | keyword(), String.t(), keyword()) ::
+          {:ok, Response.t()} | {:error, any()}
+  def upload(client \\ new(), path, opts \\ []) do
     mime_type = opts[:mime_type] || MIME.from_path(path)
     name = opts[:name] || "files/#{Path.basename(path)}"
     display_name = opts[:display_name] || Path.basename(path)
@@ -38,13 +30,9 @@ defmodule GeminiAI.Files do
       displayName: display_name
     }
 
-    headers = [
-      {"Content-Type", "application/json"}
-    ]
-
-    url
-    |> Req.post(json: body, headers: headers, params: %{key: api_key})
-    |> process_response(&from_response/1)
+    client
+    |> Req.post(url: "/files", json: body)
+    |> process_response()
   end
 
   @doc """
@@ -52,20 +40,16 @@ defmodule GeminiAI.Files do
 
   ## Examples
 
-      iex> GeminiAI.File.list()
-      {:ok, [%GeminiAI.File{...}, ...]}
-
+      iex> {:ok, files} = GeminiAI.Files.list()
+      iex> [%GeminiAI.Files.Response{} | _] = files
+      iex> length(files) > 0
+      true
   """
-  @spec list(keyword()) :: {:ok, [t()]} | {:error, any()}
-  def list(opts \\ []) do
-    api_key = Application.fetch_env!(:gemini_ai, :api_key)
-    url = "#{@base_url}/files"
-
-    params = Map.merge(%{key: api_key}, Map.new(opts))
-
-    url
-    |> Req.get(params: params)
-    |> process_response(fn %{"files" => files} -> Enum.map(files, &from_response/1) end)
+  @spec list(Req.Request.t() | keyword(), keyword()) :: {:ok, [Response.t()]} | {:error, any()}
+  def list(client \\ new(), opts \\ []) do
+    client
+    |> Req.get(url: "/files", params: Map.new(opts))
+    |> process_response(fn %{"files" => files} -> Enum.map(files, &Response.from_map/1) end)
   end
 
   @doc """
@@ -73,19 +57,18 @@ defmodule GeminiAI.Files do
 
   ## Examples
 
-      iex> GeminiAI.File.get("files/sample-image")
-      {:ok, %GeminiAI.File{...}}
-
+      iex> {:ok, file} = GeminiAI.Files.get("files/sample-image")
+      iex> %GeminiAI.Files.Response{name: "files/sample-image"} = file
+      iex> is_binary(file.mimeType)
+      true
   """
-  @spec get(String.t()) :: {:ok, t()} | {:error, any()}
-  def get(name) do
-    api_key = Application.fetch_env!(:gemini_ai, :api_key)
+  @spec get(Req.Request.t() | keyword(), String.t()) :: {:ok, Response.t()} | {:error, any()}
+  def get(client \\ new(), name) do
     name = if String.contains?(name, "/"), do: name, else: "files/#{name}"
-    url = "#{@base_url}/#{name}"
 
-    url
-    |> Req.get(params: %{key: api_key})
-    |> process_response(&from_response/1)
+    client
+    |> Req.get(url: "/#{name}")
+    |> process_response()
   end
 
   @doc """
@@ -93,37 +76,40 @@ defmodule GeminiAI.Files do
 
   ## Examples
 
-      iex> GeminiAI.File.delete("files/sample-image")
+      iex> GeminiAI.Files.delete("files/sample-image")
       :ok
-
   """
-  @spec delete(String.t()) :: :ok | {:error, any()}
-  def delete(name) do
-    api_key = Application.fetch_env!(:gemini_ai, :api_key)
+  @spec delete(Req.Request.t() | keyword(), String.t()) :: :ok | {:error, any()}
+  def delete(client \\ new(), name) do
     name = if String.contains?(name, "/"), do: name, else: "files/#{name}"
-    url = "#{@base_url}/#{name}"
 
-    url
-    |> Req.delete(params: %{key: api_key})
+    client
+    |> Req.delete(url: "/#{name}")
     |> process_response(fn _ -> :ok end)
   end
 
-  defp process_response(result, on_success \\ & &1) do
-    case result do
-      {:ok, %{status: 200, body: body}} ->
-        {:ok, on_success.(body)}
+  def new(opts \\ []) do
+    api_key = Keyword.get_lazy(opts, :api_key, &fetch_api_key/0)
 
-      {:ok, %{status: status, body: error_body}} ->
-        {:error, "HTTP #{status}: #{inspect(error_body)}"}
-
-      {:error, error} ->
-        {:error, error}
-    end
+    Req.new(
+      base_url: @base_url,
+      params: [key: api_key],
+      retry: :transient,
+      json: true
+    )
   end
 
-  defp from_response(response) do
-    struct(__MODULE__, response)
+  defp fetch_api_key do
+    Application.fetch_env!(:gemini_ai, :api_key)
   end
 
-  defstruct [:name, :mimeType, :size, :displayName, :createTime, :updateTime]
+  defp process_response({:ok, %{status: 200, body: body}}, on_success \\ &Response.from_map/1) do
+    {:ok, on_success.(body)}
+  end
+
+  defp process_response({:ok, %{status: status, body: body}}, _) do
+    {:error, "HTTP #{status}: #{inspect(body)}"}
+  end
+
+  defp process_response({:error, _} = error, _), do: error
 end
