081cd9e83cc066ef3702815542dcd2444cd01767
ontofractal <v@ontofractal.com>
2024-11-07 19:57:50 +0100

    rename module

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GeminiAI.MixProject do
 
   def project do
     [
-      app: :gemini_ai,
+      app: :geminiex,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
